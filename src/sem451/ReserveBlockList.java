ba41914f3b82d02f993fb2ab794e45999903d113
Saad-alqahtani <152808950+Saad-alqahtani@users.noreply.github.com>
2023-12-10 01:25:00 +0300

    Update ReserveBlockList.java

--- src/sem451/ReserveBlockList.java
+++ src/sem451/ReserveBlockList.java
@@ -1,54 +1,88 @@
 package sem451;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ReserveBlockList {
-    private Map<String, Person> blockedPeople;
+    // A map to store ReserveBlock objects with a String key
+    private Map<String, ReserveBlock> reserveBlocks;
 
+    // Constructor to initialize the reserveBlocks map
     public ReserveBlockList() {
-        blockedPeople = new HashMap<>();
+        reserveBlocks = new HashMap<>();
     }
 
-    public boolean addBlockedPerson(Person person) {
-        if (person == null || blockedPeople.containsKey(person.getId())) {
+    // Method to add a ReserveBlock to the map
+    public boolean addReserveBlock(ReserveBlock block) {
+        // Check if the block is null to avoid adding invalid data
+        if (block == null) {
             return false;
         }
-        person.setBlocked(true);
-        blockedPeople.put(person.getId(), person);
+
+        // Create a unique key for the block
+        String key = createCompositeKey(block);
+
+        // Check if a block with the same key already exists
+        if (reserveBlocks.containsKey(key)) {
+            return false;
+        }
+
+        // Add the block to the map with its unique key
+        reserveBlocks.put(key, block);
         return true;
     }
 
-    public boolean removeBlockedPerson(String id) {
-        if (!blockedPeople.containsKey(id)) {
+    // Method to remove a ReserveBlock from the map
+    public boolean removeReserveBlock(ReserveBlock block) {
+        // Check if the block is null
+        if (block == null) {
             return false;
         }
-        blockedPeople.remove(id).setBlocked(false);
+
+        // Create a unique key for the block
+        String key = createCompositeKey(block);
+
+        // Check if the block exists in the map
+        if (!reserveBlocks.containsKey(key)) {
+            return false;
+        }
+
+        // Remove the block from the map
+        reserveBlocks.remove(key);
         return true;
     }
 
-    public boolean isPersonBlocked(String id) {
-        return blockedPeople.containsKey(id);
+    // Method to retrieve a ReserveBlock based on date, time, and room
+    public ReserveBlock getReserveBlock(LocalDate date, int clock, Room room) {
+        // Create a unique key based on date, time, and room
+        String key = createCompositeKey(date, clock, room);
+        // Retrieve and return the block from the map
+        return reserveBlocks.get(key);
     }
 
-    public Person getBlockedPerson(String id) {
-        return blockedPeople.get(id);
-    }
-
-    public void printBlockedPeople() {
-        for (Person p : blockedPeople.values()) {
-            System.out.println(p);
+    // Method to print all ReserveBlocks
+    public void printAllReserveBlocks() {
+        for (ReserveBlock block : reserveBlocks.values()) {
+            System.out.println(block);
         }
     }
 
-    public int countBlockedPeople() {
-        return blockedPeople.size();
+    // Method to count the number of ReserveBlocks
+    public int countReserveBlocks() {
+        return reserveBlocks.size();
     }
 
-    public void clearAllBlockedPeople() {
-        for (Person p : blockedPeople.values()) {
-            p.setBlocked(false);
-        }
-        blockedPeople.clear();
+    // Method to clear all ReserveBlocks from the map
+    public void clearAllReserveBlocks() {
+        reserveBlocks.clear();
+    }
+
+    // Helper method to create a composite key for a ReserveBlock
+    private String createCompositeKey(ReserveBlock block) {
+        return createCompositeKey(block.getDate(), block.getClock(), block.getRoom());
+    }
+
+    // Helper method to create a composite key from date, time, and room
+    private String createCompositeKey(LocalDate date, int clock, Room room) {
+        return date.toString() + "_" + clock + "_" + room.getName();
     }
 }
